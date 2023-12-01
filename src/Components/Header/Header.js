b132ede7a0b715b7d5ebdf1c6c927d2f0862b89f
octaviofitz <octaviofitz@gmail.com>
2023-11-30 21:24:33 -0300

    font changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,7 +14,7 @@ function Header() {
     return (
         <section className='header'>
 <nav className="navbar">
-      <div className="logo">Galidé</div>
+      <div className="logo">GALIDÉ</div>
       <div className="menu-icon" onClick={toggleMenu}>
         ☰
       </div>
