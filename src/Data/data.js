aa93ebd0190356158d27c57f0a1bcedb7608ad94
octaviofitz <octaviofitz@gmail.com>
2023-12-15 16:19:38 -0300

    change data services

--- src/Data/data.js
+++ src/Data/data.js
@@ -4,28 +4,28 @@ const servicios = [
     img: '/img/Servicios/protect.png',
     texto: 'Nuestro servicio integral de protección de marca garantiza la reputación, imagen y control de precios de nuestros valiosos clientes. Preservamos la identidad y el prestigio de una marca, desarrollando estrategias sólidas y eficientes para enfrentar los desafíos del mercado actual.',
     cel: 541143993437,
-    msg: 'Este es un mensaje para Clato'
+    msg: 'Hola, estoy en interesado en averiguar por los servicios de Protección de Marca de Galidé.'
     },
     {id: 2,
     titulo: 'Redes Sociales',
     img: '/img/Servicios/mkt.png',
     texto: 'Brindamos gestión completa de redes sociales. Administración de cuentas, creación de contenido innovador y atractivo para potenciar la presencia en redes. Confía en nosotros para impulsar tu marca y llegar a tu audiencia de manera efectiva.',
     cel: 541132535790,
-    msg: 'Este es un mensaje para Ludmila'
+    msg: 'Hola, estoy en interesado en averiguar por los servicios de Redes Sociales de Galidé.'
     },
     {id: 3,
     titulo: 'SEO y WEB',
     img: '/img/Servicios/seo.png',
     texto: 'Impulsa tu presencia en línea con SEO y SEM. Optimizamos tu web con estrategias de búsqueda orgánica y gestionamos campañas publicitarias efectivas. Desarrolla tu web con tecnologías y lenguajes de programación modernos, funcionales y atractivos.',
-    cel: 541132535790,
-    msg: 'Este es un mensaje para Ludmila'
+    cel: 541143993437,
+    msg: 'Hola, estoy en interesado en averiguar por los servicios de SEO y SEM de Galidé.'
                 },
     {id: 4,
     titulo: 'Identidad de Marca',
     img: '/img/Servicios/web.png',
-    texto: 'Creamos una identidad de marca distintiva. Desarrollamos desde el naming y el diseño de logo hasta cada característica que pueda aportar a la marca. Dando personalidad y estilo diferenciales.',
-    cel: 541143993437,
-    msg: 'Este es un mensaje para Clato'
+    texto: 'Construimos el adn de tu marca, desde el concepto hasta la identidad visual. Tenemos en cuenta tu público objetivo y competencia para la creación de la misma. Nuestro desarrollo incluye arquetipos de diseño, naming, logotipo, slogan, paleta de colores y mucho más.',
+    cel: 541132535790,
+    msg: 'Hola, estoy en interesado en averiguar por los servicios de Identidad de Marca de Galidé.'
         }
 ]
 
