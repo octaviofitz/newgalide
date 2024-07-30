ba2715a9d806c305262ed09fb6713ca7a7f54d29
octaviofitz <octaviofitz@gmail.com>
2024-07-29 23:55:49 -0300

    navbar link english

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,72 +11,83 @@ function Header() {
     setShowMenu(!showMenu);
   };
 
-    return (
-        <section className='header'>
-<nav className="navbar">
-      <img src='/img/banner/logo.webp' className="logo" alt='Logo Galidé'></img>
-      <div className="menu-icon" onClick={toggleMenu}>
-        ☰
+  const handleExternalLink = () => {
+    window.open("https://galidemkt.com", "_blank");
+  };
+
+  return (
+    <section className='header'>
+      <nav className="navbar">
+        <img src='/img/banner/logo.webp' className="logo" alt='Logo Galidé'></img>
+        <div className="menu-icon" onClick={toggleMenu}>
+          ☰
+        </div>
+        <ul className={`menu ${showMenu ? 'show' : ''}`}>
+          <li className="menu-item">
+            <Link
+              to="servicios"
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500}
+            >
+              Servicios
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link
+              to="clientes"
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500}
+            >
+              Clientes
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link
+              to="quienesSomos"
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500}
+            >
+              Nosotros
+            </Link>
+          </li>
+          <li className="menu-item">
+            <Link
+              to="contacto"
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500}
+            >
+              Contacto
+            </Link>
+          </li>
+          <li className="menu-item">
+            <span onClick={handleExternalLink}>
+              English <img className='spanish' src='/img/banner/english.webp' />
+            </span>
+          </li>
+        </ul>
+      </nav>
+    
+      <div className='container'>
+        <div>
+          <img src='/img/banner/bars.svg' className='bars' alt='Barras decorativas'/>
+          <h1 className='titulo'>DESBLOQUEÁ EL POTENCIAL DEL <b className='strong'>HIPERESPACIO DIGITAL</b></h1>
+          <img src='img/banner/line.svg' className='line' alt='Líneas decorativas' />
+        </div>
+        <div>
+          <img src='/img/banner/a-circle.svg' className='circle' alt='Circulo giratorio'/>
+          <img src='/img/astronaut.webp' className='galaxy' alt='Astronauta Galidé' />
+        </div>
       </div>
-      <ul className={`menu ${showMenu ? 'show' : ''}`}>
-        <li className="menu-item">
-        <Link
-            to="servicios"
-            spy={true}
-            smooth={true}
-            offset={-70} 
-            duration={500}
-          >
-            Servicios
-            </Link>
-        </li>
-        <li className="menu-item">
-        <Link
-            to="clientes"
-            spy={true}
-            smooth={true}
-            offset={-70} 
-            duration={500}
-          >
-            Clientes
-            </Link>
-        </li>  <li className="menu-item">
-        <Link
-            to="quienesSomos"
-            spy={true}
-            smooth={true}
-            offset={-70} 
-            duration={500}
-          >
-            Nosotros
-            </Link>
-        </li>  <li className="menu-item">
-        <Link
-            to="contacto"
-            spy={true}
-            smooth={true}
-            offset={-70} 
-            duration={500}
-          >
-            Contacto
-            </Link>
-        </li>
-      </ul>
-    </nav>
-  
-        <div className='container'>
-            <div>
-            <img src='/img/banner/bars.svg' className='bars'  alt='Barras decorativas'/>
-              <h1 className='titulo'>DESBLOQUEÁ EL POTENCIAL DEL <b className='strong'>HIPERESPACIO DIGITAL</b></h1>
-              <img src='img/banner/line.svg' className='line' alt='Líneas decorativas' />
-              </div>
-            <div>
-              <img src='/img/banner/a-circle.svg'  className='circle' alt='Circulo giratorio'/>
-            <img src='/img/astronaut.webp' className='galaxy' alt='Astronauta Galidé' />
-            </div>
-            </div>
-        </section>
-    );
+    </section>
+  );
 }
 
 export default Header;
