35c70cc6c230b69c372717a6e5873cbfc5271b74
octaviofitz <octaviofitz@gmail.com>
2025-04-07 16:11:00 -0300

    clientes

--- src/Components/Clientes/Clientes.js
+++ src/Components/Clientes/Clientes.js
@@ -31,8 +31,41 @@ function Clientes() {
           <img className="rounded-image" src="/img/Clientes/rubicat.webp" alt="Rubicat" />
         </div>
         <div>
-          <img className="rounded-image" src="/img/Clientes/petba.png" alt="Petba" />
+          <img className="rounded-image" src="/img/Clientes/petba.webp" alt="Petba" />
           </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Marynor.webp" alt="Marynor" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Suzuki.webp" alt="Suzuki" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Stonecat.webp" alt="Stonecat" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Elepants.webp" alt="Elepants" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/FeriaDelLibro.webp" alt="FeriaDelLibro" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Taverniti.webp" alt="Taverniti" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/FletPack.webp" alt="FletPack" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Nutrip.webp" alt="Nutrip" />
+          </div>
+         {/*  <div>
+          <img className="rounded-image" src="/img/Clientes/Instituto.webp" alt="Instituto" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/CHR.webp" alt="Center for Human Reproduction" />
+          </div>
+          <div>
+          <img className="rounded-image" src="/img/Clientes/Soles.webp" alt="Soles" />
+          </div> */}
         
       </Slider>
     </div>
@@ -49,12 +82,56 @@ function Clientes() {
                </div>
 
                   <div>
-                   <img src='img/Clientes/petba.png' alt='Petba' className='imagenes' />
+                   <img src='img/Clientes/petba.webp' alt='Petba' className='imagenes' />
                       </div>
 
                      <div>
                        <img src='img/Clientes/rubicat.webp' alt='Rubicat' className='imagenes' />
-                       </div>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Marynor.webp" alt="Marynor" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Suzuki.webp" alt="Suzuki" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Stonecat.webp" alt="Stonecat" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Elepants.webp" alt="Elepants" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/FeriaDelLibro.webp" alt="Feria del Libro" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Taverniti.webp" alt="Taverniti" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Fletpack.webp" alt="Fletpack" className='imagenes'/>
+                         </div>
+                         
+                         <div>
+                        <img src="/img/Clientes/Nutrip.webp" alt="Nutrip" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Instituto.webp" alt="Instituto" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/CHR.webp" alt="Center for Human Reproduction" className='imagenes'/>
+                         </div>
+
+                         <div>
+                        <img src="/img/Clientes/Soles.webp" alt="Soles" className='imagenes'/>
+                         </div>
                   
                   </div>
 
