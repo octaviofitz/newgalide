51fc894ed9bb43727989f3dd1b3203145083f0c1
octaviofitz <octaviofitz@gmail.com>
2026-09-23 14:46:52 -0300

    politicas y privacidad

--- src/Components/Router/AppRouter.js
+++ src/Components/Router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -9,21 +9,31 @@ import Clientes from '../Clientes/Clientes';
 import Contacto from '../Contacto/Contacto';
 import QuienesSomos from '../QuienesSomos/QuienesSomos';
 import WhatsAppButton from '../Utilities/Whatsapp/WhatsappButton';
+import PoliticasYPrivacidad from '../Utilities/Politicas/Politicas';
 
-function AppRouter() {
+function Landing() {
     return (
-        <div>
-            <BrowserRouter>
-            <Header />
-            <main>
+        <main>
             <Nosotros />
             <Servicios />
             <Clientes />
             <QuienesSomos />
             <Contacto />
-            </main>
-            <Footer />
-            <WhatsAppButton phoneNumber='+5491143993437' />
+        </main>
+    );
+}
+
+function AppRouter() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/politicas-y-privacidad" element={<PoliticasYPrivacidad />} />
+                </Routes>
+                <Footer />
+                <WhatsAppButton phoneNumber='+5491143993437' />
             </BrowserRouter>
         </div>
     );
